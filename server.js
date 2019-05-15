3fa3e10a304f5655f5653f5b8466bf7edc278fae
kallard <contact@allard-kevin.fr>
2019-05-15 16:41:26 +0200

    feat: ajout de socket

--- server.js
+++ server.js
@@ -3,9 +3,13 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
 const PORT = process.env.NODE_PORT || 5000;
 const mongoURI = require('./config/keys').mongoURI;
 
+const MessageModel = require('./models/Message');
 const messagesRoutes = require('./routes/api/messages');
 
 app.use(bodyParser.json());
@@ -14,6 +18,20 @@ mongoose.connect(mongoURI, { useNewUrlParser: true})
   .then(() => console.log('MongoDB connected...'))
   .catch(err => console.log(err));
 
+io.on('connection', socket => {
+  console.log('Client connected.');
+  socket.on('sendMessage', message => {
+    const newMessage = new MessageModel({
+      user: '',
+      message: message.message
+    });
+
+    newMessage.save().then(message => io.sockets.emit('newMessage', message));
+
+  });
+  socket.on('disconnect', () => console.log('Client disconnected.'));
+});
+
 app.use('/api/messages', messagesRoutes);
 
-app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
