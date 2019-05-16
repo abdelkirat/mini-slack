8c561ccf266365bfc40bf0098c3d93f93cc14e67
kallard <contact@allard-kevin.fr>
2019-05-16 09:16:20 +0200

    feat: ajout config production

--- server.js
+++ server.js
@@ -37,4 +37,12 @@ io.on('connection', socket => {
 
 app.use('/api/messages', messagesRoutes);
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 server.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
