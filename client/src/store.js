cfadc565c1eb6893b5959102fa8cce4355a1a30b
kallard <contact@allard-kevin.fr>
2019-05-16 12:43:07 +0200

    :bug: Fixing a bug.

--- client/src/store.js
+++ client/src/store.js
@@ -1,12 +1,13 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware} from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
 const initalState = {};
 const middleware = [thunk];
-const store = createStore(rootReducer, initalState, compose(
+const store = createStore(rootReducer, initalState, composeWithDevTools(
   applyMiddleware(...middleware),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 ));
 
 export default store;
